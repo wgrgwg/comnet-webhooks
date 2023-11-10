ffde1d1cd839ffccac6b551278d27427c238d98e
wgrgwg <kyoeng14697@naver.com>
2023-11-10 18:33:13 +0900

    changed 18:30

--- tcp-server.py
+++ tcp-server.py
@@ -17,7 +17,7 @@ while 1:
     print('Client address: ', addr)
     data = conn.recv(BUFFER_SIZE)
     #if not data: break
-    currentTime = " " + " no, updated again " + time.ctime(time.time()) + "\r\n"
+    currentTime = " " + " updated after 18:30 " + time.ctime(time.time()) + "\r\n"
     print(data.decode('utf-8'))
     data = data + currentTime.encode('ascii')
     conn.send(data) # echo
