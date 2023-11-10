8ca30f0d5b5747e246b4624342a953433fa34bc6
wgrgwg <126789962+wgrgwg@users.noreply.github.com>
2023-11-10 15:37:42 +0900

    Add files via upload

---
+++ webhooks.py
@@ -0,0 +1,22 @@
+from flask import Flask, json
+from flask_restful import Resource, api
+
+import os
+
+buildBranch = 'main'
+buildPath = '/home/wgrgwg97/lab-socket-programming/'
+
+buildCommand = 'cd' + buildPath + ' && git pull origin ' + buildBranch
+
+app = Flask(__name__)
+api = Api(app)
+
+class setDeploy(Resource):
+    def post(self):
+        os.system(buildCommand)
+        return {'status' : 'success'}
+    
+api.add_resource(setDeploy, '/deploy')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
